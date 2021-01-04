a5bef34a25ca26abdc2dc6b62a5fa52062e8c298
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-04 17:20:02 +0700

    add authroutes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const todoRoutes = require('./todoRoutes')
+const authRoutes = require('./authRoutes')
 
+router.use(authRoutes)
 router.use('/todos',todoRoutes)
 
 module.exports = router
