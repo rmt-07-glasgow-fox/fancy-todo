c7c7158814696a9941c348d844a197e2e280f645
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-04 17:20:46 +0700

    add Users table and user model with association

--- server/models/todo.js
+++ server/models/todo.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      ToDo.belongsTo(models.User)
     }
   };
   ToDo.init({
