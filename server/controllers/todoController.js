b9dad7918e4a2ddd5496e8c347610830a84236ab
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 22:20:24 +0700

    add PUT /todos/:id handler

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -28,7 +28,7 @@ class todoController {
           return res.status(400).json({ message: err.errors[0].message });
         }
         // Handle error from server
-        return res.status(500).json({ message: err.message });
+        return res.status(500).json({ message: err });
       })
   }
 
@@ -43,6 +43,36 @@ class todoController {
       })
       .catch(err => {
         return res.status(404).json(err);
+      });
+  }
+
+  static putTodoHandler(req, res) {
+    const id = req.params.id;
+    const dataUpdateTodo = {
+      title: req.body.title,
+      description: req.body.description,
+      status: req.body.status,
+      due_date: req.body.due_date
+    }
+    Todo.update(dataUpdateTodo, {
+      returning: true,
+      where: { id }
+    })
+      .then(dataTodo => {
+        if (!dataTodo || dataTodo[0] === 0) {
+          // Handle error from null
+          return res.status(404).json({ message: 'Error, data not found' });
+        }
+        // Get data Object
+        return res.status(200).json(dataTodo[1][0]);
+      })
+      .catch(err => {
+        if (err.errors) {
+          // Handle error from validation
+          return res.status(400).json({ message: err.errors[0].message });
+        }
+        // Handle error from server
+        return res.status(500).json({ message: err });
       });
   }
 }
