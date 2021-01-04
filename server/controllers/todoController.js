942d8f1f14c8498ec3e3f9a9c938e2facf13dd8b
kemasdika <kmshanif@gmail.com>
2021-01-04 22:08:47 +0700

    server -get,getId,post,put,patch,delete

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const {Todo} = require('../models')
 
 class TodoController {
@@ -9,6 +10,18 @@ class TodoController {
         })
         .catch(err => {
             res.status(500).json({message:'internal server error'})
+        })
+    }
+
+    static todoByid (req, res) {
+        Todo.findOne({
+            where:{id: req.params.id}
+        })
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(404).json({message:'data not found'})
         })
     }
 
@@ -26,6 +39,54 @@ class TodoController {
         .catch(err => {
             res.status(500).json({message:'internal server error'})
         })
+    }
+    
+    static updateTodo (req,res) {
+        let todoObj = {
+            title : req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            due_date: req.body.due_date
+        }
+        Todo.update(todoObj,{
+            where:{id:req.params.id}
+        })
+        .then(data =>{
+            res.status(200).json(data)
+        })
+        .catch(err =>{
+            res.status(500).json({message:'internal server error'})
+        })
+    }
+
+    static statusChange(req,res) {
+        let todoObj = {
+            status : req.body.status
+        }
+        Todo.update(todoObj,{where:{id:req.params.id}})
+        .then(data =>{
+            res.status(200).json(data)
+        })
+        .catch(err =>{
+            res.status(404).json({message:'data not found'})
+        })
+    }
+
+    static deleteTodo (req, res) {
+        Todo.destroy({where:{id:req.params.id}})
+        .then(data => {
+            if(data == 1) {
+                res.status(200).json({message: 'data deleted sucessfully'})
+            } else {
+                throw {
+                    message : 'data not found'
+                }
+            }
+        })
+        .catch(err => {
+            res.status(404).json(err)
+        })
+
     }
 }
 
--- server/routes/todoRoute.js
+++ server/routes/todoRoute.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const TodoController = require('../controllers/todoController')
 
 router.get("/",TodoController.todoPage)
+router.get("/:id",TodoController.todoByid)
 router.post("/",TodoController.createTodo)
+router.put("/:id",TodoController.updateTodo)
+router.patch("/:id",TodoController.statusChange)
+router.delete("/:id",TodoController.deleteTodo)
 
 module.exports = router
