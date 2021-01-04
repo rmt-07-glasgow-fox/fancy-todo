e688028a0c3fbac8e07d9c7a6ffc461a09df6633
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 21:04:09 +0700

    add Post todo data code for save to database

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,7 +1,7 @@
 const { Todo } = require('../models');
 
 class todoController {
-  static getAssetHandlers(req, res) {
+  static getTodoHandler(req, res) {
     Todo.findAll()
       .then(dataTodo => {
         res.status(201).json(dataTodo);
@@ -9,6 +9,27 @@ class todoController {
       .catch(err => {
         res.status(500).json({ message: 'Internal Server Error' });
       });
+  }
+
+  static postTodoHandler(req, res){
+    const dataInputTodo = {
+      title: req.body.title,
+      description: req.body.description,
+      status: false,
+      due_date: req.body.due_date
+    }
+    Todo.create(dataInputTodo)
+      .then(dataTodo => {
+        return res.status(201).json(dataTodo);
+      })
+      .catch(err => {
+        if(err.errors){
+          // Handle error from validation
+          return res.status(400).json({ message: err.errors[0].message });
+        }
+        // Handle error from server
+        return res.status(500).json({ message: err.message });
+      })
   }
 }
 
