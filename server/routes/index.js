18db9c806e22811d06a7438769c5c52cbe4d1722
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 22:19:14 +0700

    add PUT /todos/:id router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const Controller = require('../controllers/todoController.js');
 
 router.get('/todos', Controller.getAllTodoHandler);
 router.get('/todos/:id', Controller.getOneTodoHandler);
+router.put('/todos/:id', Controller.putTodoHandler);
 router.post('/todos', Controller.postTodoHandler);
 
 module.exports = router;
