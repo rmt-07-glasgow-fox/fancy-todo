cba19a8b0948ab9707e935430735dc7f60c71631
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-04 14:57:08 +0700

    add validation

--- server/models/todo.js
+++ server/models/todo.js
@@ -14,10 +14,62 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   ToDo.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    status: DataTypes.BOOLEAN,
-    due_date: DataTypes.DATE
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        notEmpty: {
+          msg: 'Title field is required'
+        }
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        notEmpty: {
+          msg: 'Description field is required'
+        }
+      }
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate:{
+        notEmpty: {
+          msg: 'Status field is required'
+        }
+      }
+    },
+    due_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate:{
+        notEmpty: {
+          msg: 'Due Date field is required'
+        },
+        isDate:{
+          msg: 'Data type must be date'
+        },
+        dateValidate(value){
+          let now = new Date()
+          let day = now.getDate()
+          let month = now.getMonth() + 1
+          let year = now.getFullYear()
+          if(value.getFullYear() < year){
+            throw new Error('Date must be greather than today')
+          }else{
+            if(value.getMonth() + 1 < month){
+              throw new Error('Date must be greather than today')
+            }else{
+              if(value.getDate() < day){
+                throw new Error('Date must be greather than today')
+              }
+            }
+          }
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'ToDo',
