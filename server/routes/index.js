f960f39a5dd7512d03301cf9e59f787b0e68d471
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 22:57:10 +0700

    add PATCH /todos/:id route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ const Controller = require('../controllers/todoController.js');
 router.get('/todos', Controller.getAllTodoHandler);
 router.get('/todos/:id', Controller.getOneTodoHandler);
 router.put('/todos/:id', Controller.putTodoHandler);
+router.patch('/todos/:id', Controller.patchTodoHandler);
 router.post('/todos', Controller.postTodoHandler);
 
 module.exports = router;
