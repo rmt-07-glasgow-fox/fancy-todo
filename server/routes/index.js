e1bbe897d61e4875d7fb40feb7a12b3eb372da82
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 23:28:11 +0700

    add Delete /todos/:id route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ router.get('/todos', Controller.getAllTodoHandler);
 router.get('/todos/:id', Controller.getOneTodoHandler);
 router.put('/todos/:id', Controller.putTodoHandler);
 router.patch('/todos/:id', Controller.patchTodoHandler);
+router.delete('/todos/:id', Controller.deleteTodoHandler);
 router.post('/todos', Controller.postTodoHandler);
 
 module.exports = router;
