06aa7fe1002e769520f2bcd3aa1eca268de88abb
ObedAdhi <obedadhinatapraya@yahoo.com>
2021-01-04 19:47:08 +0700

    done put/todos/:id

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -4,15 +4,15 @@ const {
 
 class TodoController {
   static createTask (req, res) {
-    let data = {
+    let input = {
       title: req.body.title,
       description: req.body.description,
       status: req.body.status,
       due_date: req.body.due_date,
     }
-    Todo.create(data)
-    .then(out => {
-      res.status(201).json(out)
+    Todo.create(input)
+    .then(data => {
+      res.status(201).json(data)
     })
     .catch(err => {
       if (err.name === "SequelizeValidationError") {
@@ -38,9 +38,32 @@ class TodoController {
     Todo.findByPk(id)
     .then(data => {
       if (data) {
-        res.status(201).json(data)
+        res.status(200).json(data)
       } else if (!data) {
         res.status(404).json({message: "data not found"})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: "server error"})
+    })
+  }
+
+  static updateTask (req, res) {
+    let id = req.params.id
+    let input = {
+      title: req.body.title,
+      description: req.body.description,
+      status: req.body.status,
+      due_date: req.body.due_date,
+    }
+    Todo.update(input, {
+      where: {id}
+    })
+    .then(data => {
+      if (data == 0) {
+        res.status(404).json({message: "data not found"})
+      } else {
+        res.status(200).json( {...{id: id}, ...input})
       }
     })
     .catch(err => {
@@ -52,12 +75,25 @@ class TodoController {
     })
   }
 
-  static updateTask (req, res) {
-
-  }
-
   static modifyTask (req, res) {
-
+    // let id = req.params.id
+    // let status= req.body.status
+  
+    // Todo.findByPk(id)
+    // .then(data => {
+    //   if (data == 0) {
+    //     res.status(404).json({message: "data not found"})
+    //   } else {
+    //     res.status(200).json( {...{id: id}, ...input})
+    //   }
+    // })
+    // .catch(err => {
+    //   if (err.name === "SequelizeValidationError") {
+    //     res.status(400).json({message: "Date must be greater than today"})
+    //   } else {
+    //     res.status(500).json({message: "server error"})
+    //   }
+    // })
   }
 
   static deleteTask (req, res) {
