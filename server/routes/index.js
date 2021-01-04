88ccaef2a58c85f6fa81a4fc657bbda2418e6ec3
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 21:25:27 +0700

    add GET /todos/:id route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/todoController.js');
 
-router.get('/todos', Controller.getTodoHandler);
+router.get('/todos', Controller.getAllTodoHandler);
+router.get('/todos/:id', Controller.getOneTodoHandler);
 router.post('/todos', Controller.postTodoHandler);
 
 module.exports = router;
