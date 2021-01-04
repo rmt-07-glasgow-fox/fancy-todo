246434212d0f96d7b2fc34217f9c8541ace05942
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 21:26:25 +0700

    add GET /todos/:id Handler

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,7 +1,7 @@
 const { Todo } = require('../models');
 
 class todoController {
-  static getTodoHandler(req, res) {
+  static getAllTodoHandler(req, res) {
     Todo.findAll()
       .then(dataTodo => {
         res.status(201).json(dataTodo);
@@ -11,7 +11,7 @@ class todoController {
       });
   }
 
-  static postTodoHandler(req, res){
+  static postTodoHandler(req, res) {
     const dataInputTodo = {
       title: req.body.title,
       description: req.body.description,
@@ -23,13 +23,27 @@ class todoController {
         return res.status(201).json(dataTodo);
       })
       .catch(err => {
-        if(err.errors){
+        if (err.errors) {
           // Handle error from validation
           return res.status(400).json({ message: err.errors[0].message });
         }
         // Handle error from server
         return res.status(500).json({ message: err.message });
       })
+  }
+
+  static getOneTodoHandler(req, res) {
+    const id = req.params.id;
+    Todo.findOne({ where: { id } })
+      .then(dataTodo => {
+        if (!dataTodo) {
+          throw { message: 'Error, data not found' };
+        }
+        return res.status(200).json(dataTodo);
+      })
+      .catch(err => {
+        return res.status(404).json(err);
+      });
   }
 }
 
