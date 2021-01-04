5cb54418e33848e2a135d991ead92eab7aed9d15
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-04 18:25:32 +0700

    release-4-done

--- server/controllers/TodoController.js
+++ server/controllers/TodoController.js
@@ -72,11 +72,18 @@ class Controller {
             due_date: req.body.due_date
         }
 
-        Todo.update(editTodo, {
-            where: {id},
-            returning: true,
-            plain: true,
-        })
+        Todo.findByPk(id) 
+            .then(success => {
+                if (success) {
+                    return Todo.update(editTodo, {
+                        where: { id },
+                        returning: true,
+                        plain: true,
+                    })
+                } else {
+                    throw new Error('idNotFound')                    
+                }
+            })
             .then(updatedTodo => Todo.findOne({
                 where: {id: updatedTodo[1].id},
                 attributes: {
@@ -88,9 +95,12 @@ class Controller {
             })
             .catch(err => {
                 if (err.name === "SequelizeValidationError") {
-                    
+                    res.status(400).json({message: 'validation error'})
+                } else if (err.message === "idNotFound") {
+                    res.status(404).json({message: 'error not found'})
+                } else {
+                    res.status(500).json({ message: 'Internal server error' })
                 }
-                res.status(500).json({message: 'Internal server error'})
             })
     }
 
