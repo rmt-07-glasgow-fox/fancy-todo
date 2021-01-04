6dd1dbaf261b188eeae7ee4afcca7150c143d8f7
rozakus <abdulrozak.mail@gmail.com>
2021-01-04 14:39:38 +0700

    set up project, database

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(PORT, console.log(`listen to PORT : ${PORT}`))
