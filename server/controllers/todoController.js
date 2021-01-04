6698ff27f39b89cf8defab806dc8c4bc7e04e9b9
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-04 17:01:25 +0700

    [nanda] change to async await

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,183 +1,184 @@
 const { Todo } = require('../models');
 
 class TodoController {
-    static getAll(req, res) {
-        Todo.findAll()
-            .then(data => {
-                res.status(200).json({
-                    status: 'success',
-                    data
-                })
+    static async getAll(req, res) {
+        try {
+            const data = await Todo.findAll()
+            return res.status(200).json({
+                status: 'success',
+                data
             })
-            .catch(err => {
-                res.status(500).json({
-                    status: 'error',
-                    message: "Invalid server error"
-                })
+        } catch (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: "Invalid server error"
             })
+        }
     }
 
-    static get(req, res) {
-        Todo.findByPk(+req.params.id)
-            .then(data => {
-                if (!data) {
-                    res.status(404).json({
-                        status: 'error',
-                        message: 'todo not found'
-                    })
-                }
+    static async get(req, res) {
+        try {
+            const data = await Todo.findByPk(+req.params.id);
 
-                res.status(200).json({
-                    status: 'success',
-                    data
-                })
-            })
-            .catch(err => {
-                res.status(500).json({
+            if (!data) {
+                return res.status(404).json({
                     status: 'error',
-                    message: err.message
+                    message: 'todo not found'
                 })
+            }
+
+            return res.status(200).json({
+                status: 'success',
+                data
             })
+        } catch (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: err.message
+            })
+        }
     }
 
-    static store(req, res) {
+    static async store(req, res) {
         const { title, description, status, due_date } = req.body;
         const input = { title, description, status, due_date };
 
-        Todo.create(input)
-            .then(data => {
-                res.status(201).json({
-                    status: 'success',
-                    data
-                })
+        try {
+            const data = await Todo.create(input);
+
+            return res.status(201).json({
+                status: 'success',
+                data
             })
-            .catch(err => {
-                let temp = [];
 
-                if (err.errors.length > 0) {
-                    err.errors.forEach(el => {
-                        temp.push(el.message);
-                    })
+        } catch (err) {
+            let temp = [];
 
-                    res.status(400).json({
-                        status: 'error',
-                        message: temp
-                    })
-                }
-
-                res.status(500).json({
-                    status: 'error',
-                    message: err.message
+            if (err.errors.length > 0) {
+                err.errors.forEach(el => {
+                    temp.push(el.message);
                 })
-            });
+
+                return res.status(400).json({
+                    status: 'error',
+                    message: temp
+                })
+            }
+
+            return res.status(500).json({
+                status: 'error',
+                message: err.message
+            })
+        }
     }
 
-    static update(req, res) {
+    static async update(req, res) {
         const id = req.params.id;
         const { title, description, status, due_date } = req.body;
         const input = { title, description, status, due_date };
 
-        Todo.findByPk(id)
-            .then(data => {
-                if (!data) {
-                    res.status(404).json({
-                        status: 'error',
-                        message: 'todo not found'
-                    })
-                }
-                return Todo.update(input, { where: { id } })
-            })
-            .then(data => {
-                res.status(200).json({
-                    status: 'success',
-                    message: 'todo updated successfully'
-                })
-            })
-            .catch(err => {
-                let temp = [];
-
-                if (err.errors.length > 0) {
-                    err.errors.forEach(el => {
-                        temp.push(el.message);
-                    })
-
-                    res.status(400).json({
-                        status: 'error',
-                        message: temp
-                    })
-                }
-
-                res.status(500).json({
+        try {
+            const data = await Todo.findByPk(id);
+            if (!data) {
+                return res.status(404).json({
                     status: 'error',
-                    message: err.message
+                    message: 'todo not found'
                 })
-            });
+            }
+
+            await Todo.update(input, { where: { id } })
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'todo updated successfully'
+            })
+        } catch (err) {
+            let temp = [];
+
+            if (err.errors.length > 0) {
+                err.errors.forEach(el => {
+                    temp.push(el.message);
+                })
+
+                return res.status(400).json({
+                    status: 'error',
+                    message: temp
+                })
+            }
+
+            return res.status(500).json({
+                status: 'error',
+                message: err.message
+            })
+        }
     }
 
-    static updateStatus(req, res) {
+    static async updateStatus(req, res) {
         const id = req.params.id
         const { status } = req.body;
         const input = { status };
 
-        Todo.findByPk(id)
-            .then(data => {
-                if (!data) {
-                    res.status(404).json({
-                        status: 'error',
-                        message: 'todo not found'
-                    })
-                }
-                return Todo.update(input, { where: { id } })
-            })
-            .then(data => {
-                res.status(200).json({
-                    status: 'success',
-                    message: 'todo updated successfully'
-                })
-            })
-            .catch(err => {
-                let temp = [];
+        try {
+            const data = await Todo.findByPk(id)
 
-                if (err.errors.length > 0) {
-                    err.errors.forEach(el => {
-                        temp.push(el.message);
-                    })
-
-                    res.status(400).json({
-                        status: 'error',
-                        message: temp
-                    })
-                }
-
-                res.status(500).json({
+            if (!data) {
+                return res.status(404).json({
                     status: 'error',
-                    message: err.message
+                    message: 'todo not found'
                 })
-            });
+            }
+
+            await Todo.update(input, { where: { id } })
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'todo updated successfully'
+            })
+        } catch (err) {
+            let temp = [];
+
+            if (err.errors.length > 0) {
+                err.errors.forEach(el => {
+                    temp.push(el.message);
+                })
+
+                return res.status(400).json({
+                    status: 'error',
+                    message: temp
+                })
+            }
+
+            return res.status(500).json({
+                status: 'error',
+                message: err.message
+            })
+        }
     }
 
-    static destroy(req, res) {
-        Todo.findByPk(+req.params.id)
-            .then(data => {
-                if (!data) {
-                    res.status(404).json({
-                        status: 'error',
-                        message: 'todo not found'
-                    })
-                }
+    static async destroy(req, res) {
+        try {
+            const data = await Todo.findByPk(+req.params.id);
 
-                data.destroy();
-                res.status(200).json({
-                    status: 'success',
-                    message: 'todo successfully deleted'
-                })
-            })
-            .catch(err => {
-                res.status(500).json({
+            if (!data) {
+                return res.status(404).json({
                     status: 'error',
-                    message: err.message
+                    message: 'todo not found'
                 })
+            }
+
+            data.destroy();
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'todo successfully deleted'
             })
+
+        } catch (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: err.message
+            })
+        }
     }
 }
 
