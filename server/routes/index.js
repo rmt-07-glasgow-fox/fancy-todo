0b1e0068f3dd2bd5a69ff1136509001ab3d60d5e
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-04 20:57:40 +0700

    add Post /todos route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../controllers/todoController.js');
 
-router.get('/todos', Controller.getAssetHandlers);
+router.get('/todos', Controller.getTodoHandler);
+router.post('/todos', Controller.postTodoHandler);
 
 module.exports = router;
