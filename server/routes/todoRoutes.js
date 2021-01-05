69c7ee6c33f0f31c1716cfae2e41f4154154e05f
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-05 17:57:03 +0700

    add 3rdparty endpoints

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -2,11 +2,15 @@ const router = require('express').Router()
 const ToDoController = require('../controllers/ToDoController')
 const authorize = require('../middlewares/auth').authorize
 
+router.get('/news', ToDoController.getNews)
+router.get('/weather', ToDoController.getCurrentWeather)
+
 router.post('/', ToDoController.handleAddTask)
 router.get('/', ToDoController.getAllTasks)
 router.get('/:id', authorize, ToDoController.getTaskById)
 router.put('/:id', authorize, ToDoController.handleEditData)
 router.patch('/:id', authorize, ToDoController.handlePatch)
 router.delete('/:id', authorize, ToDoController.handleDelete)
+
 
 module.exports = router
