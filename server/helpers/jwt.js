077d5e13bd8d3b30f30757f873e864467bc1fbca
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-01-05 15:54:59 +0700

    Add .env security

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = "ralfFanCYToDo";
+const SECRET_KEY = process.env.SECRET_KEY;
 
 let generateToken = payload => jwt.sign(payload, SECRET_KEY);
 
-module.exports = { generateToken };
+let checkToken = token => jwt.verify(token, SECRET_KEY);
+
+module.exports = { generateToken, checkToken };
