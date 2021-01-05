7dbc2917ef81ae5e36a15939ec7d9e1fd77e334f
romizaki <rzferdiyanto@gmail.com>
2021-01-06 00:51:57 +0700

    api calendar

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router()
 const todoRouter = require('./todo')
 const authRouter = require('./auth')
 const { authenticate } = require('../middleware/auth')
+const CalendarController = require('../controllers/calendarController')
 
 router.get('/', (req, res) => {
     res.send('Hello World!')
-})
+})  
+
+router.get('/calendar', CalendarController.getCalendar)
 
 router.use('/auth',authRouter)
 router.use(authenticate)
