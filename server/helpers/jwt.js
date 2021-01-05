ef132eea53a2e0ded71c6d36c45c15777e8ac7fc
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-05 14:26:48 +0700

    edit-env-template

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 module.exports = {
     generateToken: (payload) => {
