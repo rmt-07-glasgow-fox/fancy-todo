61d6c22a71504468c790c30fd26a5d20cd13376b
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-05 11:36:10 +0700

    add verifyToken static method

--- server/helpers/jwtHelper.js
+++ server/helpers/jwtHelper.js
@@ -5,6 +5,9 @@ class jwtHelper {
     console.log(payload)
     return jwt.sign(payload, process.env.JWT_SECRET)
   }
+  static verifyToken(token){
+    return jwt.verify(token, process.env.JWT_SECRET)
+  }
 }
 
 module.exports = jwtHelper
