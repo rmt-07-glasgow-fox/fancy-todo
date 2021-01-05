8a63419edc9bf0eb3a27201223cc4ba38d2e162d
AnthonyGunardi <anthonygunardi@gmail.com>
2021-01-05 23:07:27 +0700

    Try add Google login. Still not work

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,8 @@
 const { User } = require('../models')
 const Token = require('../helpers/jsonwebtoken')
 const Password = require('../helpers/hash-password')
-// const {OAuth2Client} = require('google-auth-library');
-// const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 class UserController {
 
@@ -60,7 +60,8 @@ class UserController {
         try {
             const ticket = await client.verifyIdToken({
                 idToken: req.body.google_token,
-                audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+                audience: process.env.GOOGLE_CLIENT_ID,  
+                // Specify the CLIENT_ID of the app that accesses the backend
                 // Or, if multiple clients access the backend:
                 //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
             });
