1b5c161a386ed899a5682b4648a2433146876c22
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-05 11:35:56 +0700

    add NODE_ENV and fix unused middleware

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
-require('dotenv').config()
+if(process.env.NODE_ENV === "development"){
+  require("dotenv").config()
+}
 
 const express = require('express')
 const app = express()
 const routes = require('./routes')
 const port = process.env.PORT || 3000
 
-app.use(express())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
