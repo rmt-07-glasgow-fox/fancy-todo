bae604667d372577b64cca46fc363a1aabb3eec5
rozakus <abdulrozak.mail@gmail.com>
2021-01-05 08:32:36 +0700

    add updateTodos, updateStatus, deleteTodos

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,31 +10,97 @@ class Controller {
 
         try {
             let data = await Todo.create(body)
-            res.status(200).send(data)
+            res.status(201).send(data)
         } catch (err) {
             let errorMessage = err.errors ? err.errors.map(error => error.message) : ''
-            res.status(500).send(errorMessage)
+            res.status(400).send(errorMessage)
         }
     }
 
     static async todoList(req, res) {
         try {
-            let data = await Todo.findAll({ order: [['due_date', 'ASC']] })
+            let data = await Todo.findAll({
+                order: [['id', 'ASC']],
+                attributes: {
+                    // include: ['id', 'title', 'description', 'status', 'due_date'],
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            })
+
+            // console.log(data)
             res.status(200).send(data)
         } catch (err) {
-            res.status(500).send(err)
+            let errorMessage = err.errors ? err.errors.map(error => error.message) : ''
+            res.status(400).send(errorMessage)
         }
     }
 
     static async todoById(req, res) {
         try {
-            let id = req.params.id
+            let id = +req.params.id
 
-            let data = await Todo.findByPk(id)
-            res.status(200).send(data)
+            let data = await Todo.findByPk(id, {
+                attributes: {
+                    // include: ['id', 'title', 'description', 'status', 'due_date'],
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            })
+            // console.log('data : ', data)
+            data ? res.status(200).send(data) : res.status(200).send('Gak ada')
 
         } catch (err) {
-            res.status(500).send(err)
+            let errorMessage = err.errors ? err.errors.map(error => error.message) : ''
+            res.status(400).send(errorMessage)
+        }
+    }
+
+    static async todoUpdateAll(req, res) {
+        try {
+            let id = +req.params.id
+            console.log('req.body : ', req.body)
+
+            let update = {
+                title: req.body.title || '',
+                description: req.body.description || '',
+                due_date: new Date(req.body.due_date) || '',
+                status: req.body.status === 'true' ? true : false,
+            }
+
+            let response = await Todo.update(update, { where: { id } })
+            // console.log('response : ', response)
+            response[0] > 0 ? res.status(200).send('Berhasil update') : res.status(200).send('Id gak ada')
+
+        } catch (err) {
+            let errorMessage = err.errors ? err.errors.map(error => error.message) : ''
+            res.status(400).send(errorMessage)
+        }
+    }
+
+    static async todoUpdateStatus(req, res) {
+        try {
+            let id = +req.params.id
+            let update = { status: true }
+
+            let response = await Todo.update(update, { where: { id } })
+            // console.log('response : ', response)
+            response[0] > 0 ? res.status(200).send('Berhasil update status') : res.status(200).send('Id gak ada')
+
+        } catch (err) {
+            let errorMessage = err.errors ? err.errors.map(error => error.message) : ''
+            res.status(400).send(errorMessage)
+        }
+    }
+
+    static async todoDelete(req, res) {
+        try {
+            let id = +req.params.id
+
+            let deleted = await Todo.destroy({ where: { id: id } })
+            deleted ? res.sendStatus(200) : res.sendStatus(404)
+
+        } catch (err) {
+            let errorMessage = err.errors ? err.errors.map(error => error.message) : ''
+            res.status(400).send(errorMessage)
         }
     }
 }
--- server/routers/index.js
+++ server/routers/index.js
@@ -5,5 +5,8 @@ router.get('/', Controller.home)
 router.get('/todos', Controller.todoList)
 router.post('/todos', Controller.todoAdd)
 router.get('/todos/:id', Controller.todoById)
+router.put('/todos/:id', Controller.todoUpdateAll)
+router.patch('/todos/:id', Controller.todoUpdateStatus)
+router.delete('/todos/:id', Controller.todoDelete)
 
 module.exports = router
