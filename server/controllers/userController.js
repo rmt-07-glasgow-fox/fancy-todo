50a0ddeb4c67541315aef38b4ccc92efda12c4f8
cakra bilisairo <cakra.bilisairo01@gmail.com>
2021-01-05 08:58:28 +0700

    done login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const {User} = require('../models')
+const comparePassword = require('../helper/comparePass')
+const generateToken = require('../helper/jwt')
 
 class Controller{
     static register(req, res){
@@ -13,6 +15,32 @@ class Controller{
         })
         .catch(err=>{
             res.status(400).json({message: err})
+        })
+    }
+    static login(req, res){
+        let body = req.body
+        let dataUser = {
+            email: body.email,
+            password: body.password
+        }
+        User.findOne({where:{email:dataUser.email}})
+        .then(data=>{
+            if(!data){
+                res.status(401).json({message: 'Email/Password incorrect'})
+            }else{
+                let match = comparePassword(dataUser.password, data.password)
+                if(match){
+                    let payload = {id: data.id, email:data.email}
+                    let access_token = generateToken(payload)
+                    return res.status(200).json({access_token})
+                }else{
+                    res.status(401).json({message: 'Email/Password incorrect'})
+                }
+            }
+        })
+        .catch(err=>{
+            res.status(400).json(err)
+            console.log(err)
         })
     }
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Controller = require('../controllers/userController')
 
 router.post('/register', Controller.register)
+router.post('/login', Controller.login)
 
 
 module.exports = router
