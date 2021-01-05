41babb6ef13364d2d31e83450132f0b214c1fdfb
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-05 13:39:37 +0700

    use ath middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,11 @@ const router = require('express').Router()
 const todoRoutes = require('./todoRoutes')
 const authRoutes = require('./authRoutes')
 
+const authentication = require('../middlewares/auth').authentication
+
 router.use(authRoutes)
+
+router.use(authentication)
 router.use('/todos',todoRoutes)
 
 module.exports = router
