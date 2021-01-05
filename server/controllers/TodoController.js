cf769eeafa0d7a9273cd8e813169fc60d941f849
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-05 07:07:38 +0700

    done

--- server/controllers/TodoController.js
+++ server/controllers/TodoController.js
@@ -1,7 +1,168 @@
 const { Todo } = require('../models');
 
 class TodoController {
- 
-}
+    static getTodos(req, res) {
+        Todo.findAll({
+            order: [['id', 'ASC']],
+            attributes: {
+                exclude: ['createdAt','updatedAt']
+            }
+        })
+            .then(todos => res.status(200).json(todos))
+            .catch(err => res.status(500).json({message: 'Internal server error'}))
+    }
 
-module.exports = TodoController
+    static addTodo(req, res) {
+        let newTodo = {
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            due_date: req.body.due_date
+        }
+
+        Todo.create(newTodo, {
+            attributes: {
+                exclude: ['createdAt','updatedAt']
+            }
+        })
+            .then(todo => res.status(201).json({
+                title: todo.title,
+                description: todo.description,
+                status: todo.status,
+                due_date: todo.due_date
+            }))
+            .catch(err => {
+                if (err.name === "SequelizeValidationError") {
+                    res.status(400).json({message: 'Validation error'})
+                } else {
+                    res.status(500).json({message: "Internal server error"})
+                }
+            })
+    }
+
+    static getTodo(req, res) {
+        let id = req.params.id;
+
+        Todo.findOne({
+            where: {id},
+            attributes: {
+                exclude: ['createdAt','updatedAt'],
+            }
+        })
+            .then(todo => {
+                if (todo) res.status(200).json(todo)
+                else throw new Error('Error not found')
+            })
+            .catch(err => res.status(500).json({message: err.message}))
+    }
+
+    static editTodo(req, res) {
+        let id = req.params.id;
+        let editTodo = {
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            due_date: req.body.due_date
+        }
+        
+        Todo.update(editTodo, {
+            where: {id},
+            returning: true,
+        })
+            .then(data => {
+                let success = data[0];
+                let updatedTodo = data[1][0];
+
+                if (!success) {
+                    throw new Error ('IdNotFound')
+                } else {
+                    res.status(200).json({
+                        title: updatedTodo.title,
+                        description: updatedTodo.description,
+                        status: updatedTodo.status,
+                        due_date: updatedTodo.due_date
+                    })
+                } 
+            })  
+            .catch(err => {
+                if (err.name === 'SequelizeValidationError') {
+                    res.status(400).json({message: 'Validation error'})
+                } else if (err.message === "IdNotFound") {
+                    res.status(404).json({message: 'Error not found'})
+                } else {
+                    res.status(500).json({message: 'internal server error'})
+                }
+            }) 
+    }   
+
+    static editStatusTodo(req, res) {
+        let id = req.params.id;
+        let editTodo = {
+            status: req.body.status
+        }
+
+        Todo.update(editTodo, {
+            where: {id},
+            returning: true
+        })
+            .then(data => {
+                let success = data[0];
+                let updatedTodo = data[1][0];
+
+                if (!success) {
+                    throw new Error('IdNotFound')
+                } else {
+                    res.status(200).json({
+                        title: updatedTodo.title,
+                        description: updatedTodo.description,
+                        status: updatedTodo.status,
+                        due_date: updatedTodo.due_date
+                    })
+                }
+            })
+            .catch(err => {
+                if (err.name === 'SequelizeValidationError') {
+                    res.status(400).json({ message: 'Validation error' })
+                } else if (err.message === "IdNotFound") {
+                    res.status(404).json({ message: 'Error not found' })
+                } else {
+                    res.status(500).json({ message: 'internal server error' })
+                }
+            }) 
+    }
+
+    static deleteTodo(req, res) {
+        let id = req.params.id;
+        let deletedTodo = '';
+
+        Todo.findOne({
+            where: {id},
+            attributes: {
+                exclude: ['createdAt','updatedAt']
+            }
+        })
+            .then(todo => {
+                deletedTodo = todo;
+                return Todo.destroy({ where: { id } })
+            })
+            .then(success => {
+                if (!success) {
+                    throw new Error('IdNotFound');
+                } else {
+                    res.status(200).json({
+                        message: 'Todo success to delete',
+                        data: deletedTodo
+                    })  
+                }
+            })
+            .catch(err => {
+                if (err.message === "IdNotFound") {
+                    res.status(404).json({message: 'Error not found'})
+                } else {
+                    res.status(500).json({message: 'Internal server error'}) 
+                }
+            })
+    }
+}  
+  
+module.exports = TodoController 
--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const TodoController = require('../controllers/TodoController');
 
+router.get('/', TodoController.getTodos);
+router.post('/', TodoController.addTodo);
 
+router.get('/:id', TodoController.getTodo);
+router.put('/:id', TodoController.editTodo);
+
+router.patch('/:id', TodoController.editStatusTodo);
+router.delete('/:id', TodoController.deleteTodo)
 
 module.exports = router;
