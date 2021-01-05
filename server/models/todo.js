d537d31df8d1dd6c469139ca03cfaa2415bde6c9
muhammadhaikal <muh.heykall@gmail.com>
2021-01-05 16:58:54 +0700

    add User table and relation to Todo

--- server/models/todo.js
+++ server/models/todo.js
@@ -4,13 +4,8 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Todo.belongsTo(models.User, {foreignKey: 'UserId'})
     }
   };
   Todo.init({
@@ -33,7 +28,8 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     status: DataTypes.BOOLEAN,
-    due_date: DataTypes.DATE
+    due_date: DataTypes.DATE,
+    UserId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Todo',
