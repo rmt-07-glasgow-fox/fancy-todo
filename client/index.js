dd6ada096b4984725716806ddd7c47c0508cd06d
TommySus <tommysusanto77@gmail.com>
2021-01-06 18:12:17 +0700

    commit-1-client

--- client/index.js
+++ client/index.js
@@ -1,17 +1,4 @@
 
-$(document).ready(function(){
-    // $("#form-login").on("submit", function(e){
-    //     e.preventDefault();
-    //     login()
-    // });
-    loginPage()
-});
-
-function loginPage () {
-    $("#login").hide();
-    $("#homePage").show();
-}
-
 function login(){
     const email = $("#email").val()
     const password = $("#password").val()
@@ -25,8 +12,8 @@ function login(){
         } 
     })
     .done(response => {
-        console.log(response)
         localStorage.setItem('access_token',response.access_token)
+        homePage()
     })
     .fail((xhr,textstatus) => {
         console.log(xhr,textstatus)
@@ -35,4 +22,67 @@ function login(){
         $("#email").val("")
         $("#password").val("")
     })
+}
+
+function fetchData(){
+    $("#list-todos").empty()
+    $.ajax({
+        url: "http://localhost:3000/todos",
+        method: "GET",
+        headers: {
+            access_token: localStorage.getItem('access_token')
+        }
+    })
+    .done(response => {
+        console.log(response)
+        for(let i  = 0; i < response.length; i++){
+            $("#list-todos").append(` <div id="title-todo" class="flex mx-auto">
+            <h5 class="card-title">${i + 1}. ${response[i].title}</h5>
+            </div>
+            <div id="info-todo">
+                <div class="card-body">
+                    <p class="card-text">${response[i].description}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="mx-auto list-group-item"><b>status:</b> ${response[i].status}</li>
+                    <li class="mx-auto list-group-item"><b>due date:</b> ${response[i].due_date.slice(0,10)}</li>
+                </ul>
+            </div>
+            <div class="mx-auto card-body">
+                <button onclick="formEditTodo(${response[i].id})" type="button" class="btn btn-primary btn-sm">&#128393; edit</button>
+                <button id="myBtn" onclick="deleteTodo(${response[i].id})" type="button" class="btn btn-danger btn-sm">&#128465; delete</button>
+            </div>`)
+        }
+    })
+    .fail(error => {
+        console.log(error)
+    })
+}
+
+
+$(document).ready(function(){
+    if(localStorage.getItem('access_token')){
+        homePage()
+    } else {
+        loginPage()
+    }
+    $("#form-login").on("submit", function(e){
+        e.preventDefault();
+        login()
+    });
+    $("#logout-btn").click(function(e){
+        e.preventDefault();
+        localStorage.removeItem('access_token')
+        loginPage()
+    });
+});
+
+function loginPage () {
+    $("#login").show();
+    $("#homePage").hide();
+}
+function homePage () {
+    $("#login").hide();
+    $("#homePage").show();
+    fetchData()
 }
