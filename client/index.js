7e4019d4eb428d3517cf8ea2a2d05d4c0a918217
TommySus <tommysusanto77@gmail.com>
2021-01-06 15:01:15 +0700

    add-client

---
+++ client/index.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+    $("#form-login").on("submit", function(e){
+        e.preventDefault();
+        login()
+    });
+
+});
+
+
+function login(){
+    const email = $("#email").val()
+    const password = $("#password").val()
+    $.ajax({
+        url: 'http://localhost:3000/users/login',
+        method: 'POST',
+        timeout: 1000,
+        data: {
+            email,
+            password
+        } 
+    })
+    .done(response => {
+        console.log(response)
+        localStorage.setItem('access_token',response.access_token)
+    })
+    .fail((xhr,textstatus) => {
+        console.log(xhr,textstatus)
+    })
+    .always(_ => {
+        // $("#email").val("")
+        // $("#password").val("")
+    })
+}
--- server/app.js
+++ server/app.js
@@ -7,9 +7,13 @@ const app = express()
 const port = 3000
 const routes = require('./routes/index')
 const errorHandler = require('./middleware/errorHandler')
+const cors = require('cors')
 
 
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
 
 app.use("/",routes)
 app.use(errorHandler)
