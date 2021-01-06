701fb6e914df17b9b68e5a6d519f1ce46b318ce1
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-07 02:38:41 +0700

    [nanda] fix bug datatables, delete function todos and update status todos

--- client/js/myScript.js
+++ client/js/myScript.js
@@ -4,38 +4,110 @@ let tableTodo;
 $(document).ready(function() {
     if (localStorage.access_token) {
         dashboardPage()
-        tableTodo = $('#tableTodo').DataTable({
-            searchable: true,
-            processing: true,
-            language: {
-                "processing": '<div class="spinner-border text-info m-2" role="status"><span class="sr-only"></span></div></br><div>Tunggu Sebentar yaa...</div>',
-                "paginate": {
-                    "previous": "<i class='uil uil-angle-left'>",
-                    "next": "<i class='uil uil-angle-right'>"
-                }
-            },
-            "drawCallback": function() {
-                $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
-            },
-            ajax: {
-                method: 'GET',
-                url: `${baseUrl}/todos`,
-                headers: {
-                    authorization: localStorage.access_token
-                },
-            },
-            columns: [
-                { "data": "id" },
-                { "data": "title" },
-                { "data": "description" },
-                { "data": "status" }
-            ]
-        });
-        console.log(tableTodo);
     } else {
         loginPage()
     }
     $("#registerPassword, #registerRepeatPassword").keyup(checkPasswordMatch);
+
+    $('#btnCreate').click(function() {
+        $('#mcreate').modal();
+    });
+});
+
+
+const tableTodosFetch = () => {
+    tableTodo = $('#tableTodo').DataTable({
+        destroy: true,
+        searchable: true,
+        processing: true,
+        async: false,
+        language: {
+            "processing": '<div class="spinner-border text-info m-2" role="status"><span class="sr-only"></span></div></br><div>Tunggu Sebentar yaa...</div>',
+        },
+        "drawCallback": function() {
+            $('.dataTables_paginate > .pagination').addClass('pagination-rounded');
+        },
+        ajax: {
+            method: 'GET',
+            url: `${baseUrl}/todos`,
+            headers: {
+                authorization: localStorage.getItem('access_token')
+            },
+            error: function(err) {
+                toastr.info(`${err.responseJSON.message}`, 'session expired');
+                logout()
+            }
+        },
+        columns: [
+            { data: 'id', name: 'id', visible: false, searchable: false },
+            {
+                data: "status",
+                render: (data) => {
+                    let is_checked = data === true ? "checked" : "";
+                    let message = data === true ? "done" : "onProgress";
+                    return `<div class="custom-control custom-checkbox small "> <input type="checkbox" ${is_checked} class="custom-control-input" id="customCheck" > <label class="custom-control-label" for="customCheck" > <h6> ${message} </h6> </label> </div >`;
+                }
+            },
+            { data: "title" },
+            { data: "description" },
+            { data: 'action', name: 'action', orderable: false, searchable: false, defaultContent: '<div class="input-group-btn"><button class="btn btn-warning" id="bedit"><i class="fa fa-pencil-alt"></i></button><button class="btn btn-danger" style="margin-left:5px" id="bdestroy"><i class="fa fa-trash"></i></button> </div>' },
+        ],
+    });
+
+}
+
+$('#tableTodo tbody').on('click', '#customCheck', function() {
+    var id = tableTodo.row($(this).parents('tr')).data().id;
+    var status = tableTodo.row($(this).parents('tr')).data().status;
+
+    $.ajax({
+        type: "PATCH",
+        url: `${baseUrl}/todos/${id}`,
+        data: { status: !status },
+        headers: {
+            authorization: localStorage.access_token
+        },
+        success: (data) => {
+            toastr.success(data.message, 'Success Alert')
+            tableTodo.ajax.reload()
+        },
+        error: (err) => {
+            toastr.error(err.message, 'Error Alert')
+        }
+    });
+});
+
+$('#tableTodo tbody').on('click', '#bdestroy', function() {
+    const id = tableTodo.row($(this).parents('tr')).data().id;
+    Swal.fire({
+        title: "Apakah anda yakin?",
+        text: "data yang terhapus tidak bisa dikembalikan!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: false
+    }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                type: "DELETE",
+                url: `${baseUrl}/todos/${id}`,
+                headers: {
+                    authorization: localStorage.access_token
+                },
+                success: (data) => {
+                    Swal.fire("Done!", "Data Berhasil di hapus!", "success");
+                    toastr.success(data.message, 'Success Alert')
+                    tableTodo.ajax.reload()
+                },
+                error: (err) => {
+                    Swal.fire("Error deleting!", "Please try again", "error");
+                    toastr.error(err.message, 'Error Alert')
+                }
+            });
+        }
+    })
 });
 
 const dashboardPage = () => {
@@ -77,7 +149,7 @@ const todoPage = () => {
     if (localStorage.access_token) {
         $('#todoContent').show();
         $('#dasboardContent').hide();
-        tableTodo.ajax.reload(null, false);
+        tableTodosFetch()
     } else {
         $('#loginEmail').val('')
         $('#loginPassword').val('')
@@ -88,11 +160,14 @@ const todoPage = () => {
     }
 }
 
+const refreshTableTodo = () => {
+    tableTodo.ajax.reload(null, false);
+}
+
 const logout = () => {
     localStorage.clear()
     $('#logoutModal').modal('hide')
     loginPage()
-
 }
 
 const checkPasswordMatch = () => {
