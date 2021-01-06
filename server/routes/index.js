e412a5190172b6f0c93735e5d8ad69dd74094c50
Fauzi Ramadan <fauzidanrama@gmail.com>
2021-01-06 08:04:00 +0700

    public 3rd party api: official-joke to make a day

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const axios = require('axios')
 const todosRouter = require('./todos.js')
 const authRouter = require('./auth.js')
 const { authenticate } = require('../middlewares/auth.js')
+const { response } = require('express')
 
 router.use(authRouter)
 
@@ -9,9 +11,20 @@ router.use(authenticate)
 router.use('/todos', todosRouter)
 
 router.get('/', (req, res) => {
-    return res.status(200).json({
-        msg: "Welcome"
-    })
+    const apiUrl = "https://official-joke-api.appspot.com/jokes/programming/random"
+    axios.get(apiUrl)
+    .then( response => {
+        return res.status(200).json({
+            msg: "welcome",
+            setup: `${response.data[0].setup}`,
+            punchline: `${response.data[0].punchline}`
+        })
+    } )
+    .catch( error => {
+        return res.status(500).json({
+            msg: error.message
+        })
+    } )
 } )
 
 module.exports = router
