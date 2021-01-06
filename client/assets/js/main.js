7e70b0628ad4a1ca409f10188f1a93051ef16cb2
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-06 19:50:38 +0700

    add: google sign in data and meta

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -126,6 +126,10 @@ registerBtn.click((e) => {
 
 logoutBtn.click((e) => {
   localStorage.clear()
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
   checkAuth()
 })
 
@@ -366,5 +370,25 @@ const deleteData = (id) => {
     })
     .always(_ => {
       console.log('always')
+    })
+}
+
+function onSignIn(googleUser) {
+  const id_token = googleUser.getAuthResponse().id_token;
+  $.ajax({
+    method: 'POST',
+    url: `${baseUrl}/googleLogin`,
+    data:{
+      id_token
+    }
+  })
+    .done(res => {
+      console.log(res)
+      localStorage.setItem('access_token', res.jwtToken)
+      localStorage.setItem('email', res.userData.email)
+      checkAuth()
+    })
+    .fail(err => {
+      console.log(err)
     })
 }
