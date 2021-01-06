0b765295a92ac558d2bf8be18fe06207ef86bd25
ibnutriyuono <ibnutriyuono@gmail.com>
2021-01-06 19:51:28 +0700

    add googleLogin endpoint and static googleLogin

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models')
+const { OAuth2Client } = require('google-auth-library')
 const decryptPassword = require('../helpers/encryptAndDecrypt').decryptPassword
 const generateToken = require('../helpers/jwtHelper').generateToken
 
@@ -61,6 +62,52 @@ class UserController {
             })
           }
         }
+      })
+      .catch(err => {
+        next({
+          status: 500,
+          data: err
+        })
+      })
+  }
+  static userGoogleLogin(req, res, next){
+    const { id_token } = req.body
+    let email
+    const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+    client.verifyIdToken({
+      idToken: id_token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    })
+      .then(data => {
+        const payload = data.getPayload()
+        email = payload.email
+        return User.findOne({
+          where:{
+            email
+          }
+        })
+      })  
+      .then(user => {
+        if(!user){
+          return User.create({
+            email,
+            password: (Math.random()*1000+`${process.env.JWT_SECRET}`).toString()
+          })
+        }else{
+          return user
+        }
+      })
+      .then(data => {
+        let jwtToken = generateToken({
+          id: data.id,
+          email: data.email
+        })
+        res.status(200).json({
+          jwtToken,
+          userData: {
+            email: data.email
+          }
+        })
       })
       .catch(err => {
         next({
