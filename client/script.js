e62bba0980edb69a16e4844ddd9ea7fcf0c5c577
Helmi Sugandi <sugandihelmi@gmail.com>
2021-01-07 07:33:56 +0700

    Add update

--- client/script.js
+++ client/script.js
@@ -1,37 +1,171 @@
-var baseUrl = 'http://localhost:3000'
-$(document).ready(function() {
+// https://fancytodo-unitedfox.web.app/#
+const baseUrl = 'http://localhost:3000';
+let todoList = [];
+
+function checkAuth() {
+  if (localStorage.access_token) {
+
+    $('#login-page').hide();
+    getTodoList();
+    $('#register-page').hide();
+    $('#todo-list').show();
+    $('#logout-btn').show();
+    $('#updateTodo').hide();
+
+
+
+  } else {
+
+    $('#login-page').show();
+    $('#register-page').hide();
+    $('#todo-list').hide();
+    $('#logout-btn').hide();
+    $('#updateTodo').hide();
+
+
+  }
+}
+
+function getTodoList() {
+  $.ajax({
+    method: 'GET',
+    url: `${baseUrl}/todos`,
+    headers: {
+      access_token: localStorage.access_token
+    }
+  })
+    .done(response => {
+      // console.log(response);
+      // DOM Manipulate
+      todoList = response.map(todo => {
+        return {
+          id: todo.id,
+          UserId: todo.UserId,
+          title: todo.title,
+          description: todo.description,
+          status: todo.status,
+          due_date: new Date(todo.due_date).toISOString().split('T')[0]
+        }
+      });
+      $('#todo-list').empty();
+      todoList.forEach(todo => {
+        $('#todo-list').append(
+          `<div class="card">
+            <div class="card-header">
+              ${todo.due_date}
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">${todo.title}</h5>
+            <p class="card-text">${todo.description}</p>
+            
+            <div class="form-check" style="float: right;">
+            <input class="form-check-input" type="checkbox" value="" ${(todo.status) ? 'checked' : ''} id="status">
+            <label class="form-check-label" for="status">
+              Todo done
+            </label>
+          </div>
+            
+            <a href="#" class="btn btn-primary" onclick="getOneTodo(${todo.id})">Update</a>    
+        </div>
+      </div>`
+        )
+
+      });
+
+    })
+    .fail(err => {
+      console.log(err);
+    })
+    .always(() => {
+      console.log('ALWAYS getTodoList');
+    })
+
+}
+
+// function editStatus(status) {
+//   console.log(status, '-------');
+//   if (!status) {
+//     return $("#updateStatus").removeAttr("checked");
+//   }
+//   $("#updateStatus").attr("checked");
+
+// }
+
+function getOneTodo(id) {
+  $('#updateTodo').show();
+  // $('#todo-list').hide();
+  $.ajax({
+    method: 'GET',
+    url: `${baseUrl}/todos/${id}`,
+    headers: {
+      access_token: localStorage.access_token
+    }
+  })
+    .done(response => {
+      // TODO: HAPUS UPDATE dan VALUEnya -----------------------------
+      $('#updateTitle').val(response.title);
+      $('#updateDueDate').val(new Date(response.due_date).toISOString().split('T')[0]);
+      $('#updateDescription').val(response.description);
+      $('#update-btn').data('id', id);
+      if (!response.status) {
+        $("#updateStatus").removeAttr("checked");
+      } else {
+        $("#updateStatus").attr("checked", "checked");
+      }
+      console.log(response);
+      console.log(response.status);
+    })
+    .fail(err => {
+      console.log(err);
+    })
+    .always(() => {
+      console.log('ALWAYS One Todo');
+    })
+}
+
+
+$(document).ready(function () {
   console.log('<><><><><><><> Reload Page <><><><><><><>');
+  checkAuth();
 
-  $('#login-page').show();
-  $('#register-page').hide();
-
-  $('#registerHere-btn').click(function(event){
+  $('#registerHere-btn').click(function (event) {
     event.preventDefault()
     $('#login-page').hide();
     $('#register-page').show();
+    $('#todo-list').hide();
+
   })
-  
-  $('#loginHere-btn').click(function(event){
+
+  $('#loginHere-btn').click(function (event) {
     event.preventDefault()
     $('#login-page').show();
     $('#register-page').hide();
+    $('#todo-list').hide();
   })
 
-  $('#login-btn').click(function(event){
+  $('#logout-btn').click(function () {
+    localStorage.clear();
+    checkAuth();
+  })
+
+
+  $('#login-btn').click(function (event) {
     event.preventDefault()
-  
+
     const email = $('#email').val()
-    const password =$('#password').val()
-  
+    const password = $('#password').val()
+
     $.ajax({
-      method:'POST',
+      method: 'POST',
       url: `${baseUrl}/login`,
       data: { email, password }
     })
       .done(response => {
         console.log(response, 'RESPONSE CLIENT!!!!');
         // save token to localStorage
-        localStorage.setItem('access_token', response.access_token)
+        // localStorage.setItem('access_token', response.access_token)
+        localStorage.access_token = response.access_token;
+        checkAuth();
       })
       .fail(err => {
         console.log(err, 'ERROR CLIENT');
@@ -39,11 +173,13 @@ $(document).ready(function() {
       })
       .always(() => {
         console.log('ALWAYS');
+        $('#email').val('');
+        $('#password').val('');
       })
-  
+
   })
 
-  $('#register-btn').click(function(event) {
+  $('#register-btn').click(function (event) {
     event.preventDefault()
     const email = $('#emailRegister').val()
     const password = $('#passwordRegister').val()
@@ -55,14 +191,61 @@ $(document).ready(function() {
     })
       .done(response => {
         console.log(response);
+        checkAuth();
       })
       .fail(err => {
         console.log(err);
       })
       .always(() => {
         console.log('ALWAYS');
+        $('#emailRegister').val('');
+        $('#passwordRegister').val('');
       })
   })
+
+  $('#update-btn').click(function (event) {
+    event.preventDefault();
+    const todoId = $('#update-btn').data('id');
+    const due_date = $('#updateDueDate').val();
+    const title = $('#updateTitle').val();
+    const description = $('#updateDescription').val();
+    const status = $('#updateStatus').is(":checked");
+    console.log(status);
+
+    console.log(todoId);
+    $.ajax({
+      method: 'PUT',
+      url: `${baseUrl}/todos/${todoId}`,
+      headers: {
+        access_token: localStorage.access_token
+      },
+      data: {
+        due_date,
+        title,
+        description,
+        status
+      }
+    })
+      .done(response => {
+        console.log(response);
+      })
+      .fail(err => {
+        console.log(err, 'ERR');
+      })
+      .always(() => {
+        console.log('ALWAYS');
+      })
+  })
+
+
+  $('#cancelUpdate-btn').click(function () {
+    $('#updateTodo').hide();
+    $('#updateDueDate').val('');
+    $('#updateTitle').val('');
+    $('#updateDescription').val('');
+    $("#updateStatus").removeAttr("checked");
+  })
+
 })
 
 
