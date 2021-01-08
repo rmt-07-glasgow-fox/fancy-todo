9d75ab020b6ac8b8444f6be8f7a35cafa770e97e
Ifan Satya <ifansatyahudaya@gmail.com>
2021-01-09 00:19:15 +0700

    add new,create,delete,edit,update client jquery

--- client/script.js
+++ client/script.js
@@ -1,4 +1,3 @@
-
 let baseUrl = "http://localhost:3000"
 let todos = []
 
@@ -13,8 +12,6 @@ $("#login-btn").click(function(event) {
   event.preventDefault()
   let email = $("#email").val()
   let password = $("#password").val()
-
-  console.log(email, password, '->>>>>>>');
 
   $.ajax({
     method: 'post',
@@ -50,17 +47,28 @@ $("#btn-logout").click(function() {
 
 $("#btn-update").click(function(event) {
   let todoId = $("#btn-update").data('id')
+  let title = $("#edit-title-value").val()
+  let description = $("#edit-description-value").val()
+  let status = $("#edit-status-value").val()
+  let duedate = $("#edit-duedate-value").val()
+
   event.preventDefault()
   $.ajax({
     method: 'put',
     url: `${baseUrl}/todos/${todoId}`,
     headers: {
       access_token: localStorage.access_token
+    },
+    data: {
+      title,
+      description,
+      status,
+      due_date: duedate
     }
   })
   .then((data) => {
-    console.log(data, 'HASIL UPDATE PUT');
     getTodolist()
+    $("#form-edit").hide()
   })
   .fail((error) => {
     console.log(error);
@@ -73,15 +81,33 @@ $("#btn-update").click(function(event) {
   })
 })
 
-// $("#link-delete").click(function() {
-//   $.ajax({
-//     method: 'delete',
-//     url: `${baseUrl}/todos/`
-//   })
-// })
+
+$("#new-todo-form").click(function() {
+  $("#todolist").hide()
+  $("#form-new").show()
+})
+
+$("#back-fromdetail-toindex").click(function() {
+  $("detail-todo").hide()
+  getTodolist
+})
+
+$("#btn-create").click(function(event) {
+  event.preventDefault()
+  let title = $("#new-title-value").val()
+  let description = $("#new-description-value").val()
+  let status = $("#new-status-value").val()
+  let duedate = $("#new-duedate-value").val()
+
+  onCreate(title, description, status, duedate)
+})
 
 
 
+
+
+
+// ===========================================
 
 
 // FUNCTION SECTION
@@ -93,7 +119,10 @@ function checkAuth() {
     $("#login-todolist").hide()
     getTodolist()
     $("#btn-logout").show()
-    $("#form-edit").show()
+    $("#form-edit").hide()
+    $("#form-new").hide()
+    $("#detail-todo").hide()
+
   } else {
     console.log('LOGIN GAGAL');
     $("#login-todolist").show()
@@ -115,6 +144,7 @@ function getTodolist() {
   .done((todos) => {
     console.log(todos);
     $("#todolist").empty()
+    $("#todolist").show()
     todos.forEach((todo) => {
       $("#todolist").append(
       `
@@ -125,8 +155,8 @@ function getTodolist() {
           <p class="card-text">${todo.status}</p>
           <p class="card-text">${todo.due_date}</p>
           <a href="#" onclick="editTodo(${todo.id})">Edit</a>
-          <a href="#">Show</a>
-          <a href="#">Delete</a>
+          <a href="#" onclick="showTodo(${todo.id})">Show</a>
+          <a href="#" onclick="onDelete(${todo.id})">Delete</a>
         </div>
       </div>
       `)
@@ -138,7 +168,22 @@ function getTodolist() {
   .always(() => {})
 }
 
+// function editTodo(id) {
+//   $("#todolist").hide()
+//   $("#new-todo-form").hide()
+//   $("#form-edit").show()
+
+//   console.log(todo);
+//   $("#edit-title-value").val(todo.title)
+//   $("#edit-description-value").val(todo.description)
+//   $("#edit-status-value").val(todo.status)
+//   $("#edit-duedate-value").val(todo.due_date)
+//   $("#btn-update").data('id', todo.id)
+// }
+
 function editTodo(id) {
+  $("#todolist").hide()
+  $("#form-edit").show()
   $.ajax({
     url: `${baseUrl}/todos/${id}`,
     headers: {
@@ -161,10 +206,11 @@ function editTodo(id) {
   })
 }
 
+
 // google function 1
 function onSignIn(googleUser) {
   const id_token = googleUser.getAuthResponse().id_token;
-  
+
   $.ajax({
     method: 'post',
     url: `http://localhost:3000/googleSignin`,
@@ -175,7 +221,6 @@ function onSignIn(googleUser) {
     checkAuth()
   })
   .fail((error) => {
-    console.log(error, 'ERRORR EUY');
   })
   .always(() => {
 
@@ -185,4 +230,76 @@ function onSignIn(googleUser) {
   // console.log('Name: ' + profile.getName());
   // console.log('Image URL: ' + profile.getImageUrl());
   // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function onCreate(title, description, status, duedate) {
+
+  $.ajax({
+    method: 'post',
+    url: `${baseUrl}/todos`,
+    headers: {
+      access_token: localStorage.access_token
+    },
+    data: {
+      title,
+      description,
+      status,
+      due_date: duedate
+    }
+  })
+  .done((todo) => {
+    $("#form-new").hide()
+    getTodolist()
+  })
+  .fail((error) => {
+  })
+  .always(() => {
+    $("#new-title-value").val('')
+    $("#new-description-value").val('')
+    $("#new-status-value").val('')
+    $("#new-duedate-value").val('')
+  })
+}
+
+function onDelete(id) {
+  $.ajax({
+    method: 'delete',
+    url: `${baseUrl}/todos/${id}`,
+    headers: {
+      access_token: localStorage.access_token
+    }
+  })
+  .done(() => {
+    getTodolist()
+  })
+  .fail((error) => {
+  })
+  .always(() => {
+
+  })
+}
+
+function showTodo(id) {
+  $("#todolist").hide()
+  $("detail-todo").show()
+  $.ajax({
+    method: 'get',
+    url: `${baseUrl}/todos/id`,
+    headers: {
+      access_token: localStorage.access_token
+    }
+  })
+  .done((todo) => {
+    $("#detail-todo-title").val(todo.title)
+    $("#detail-todo-description").val(todo.description)
+    $("#detail-todo-status").val(todo.status)
+    $("#detail-todo-duedate").val(todo.due_date)
+
+  })
+  .fail((error) => {
+    console.log(error);
+  })
+  .always(() => {
+
+  })
 }
