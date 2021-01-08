085fed74592292fcdaa4a9e3dff4fba4157f30e1
abbymakarim <abby.makarim@gmail.com>
2021-01-08 20:00:11 +0700

    will use 3rd API using calendarific to find holidays

---
+++ server/routes/calender.js
@@ -0,0 +1,14 @@
+// axios.get('https://calendarific.com/api/v2/holidays', {
+//     params : {
+//         api_key : '7c8411b089b499d0a18b199f3529038f47184a49',
+//         country : 'ID',
+//         year : 2021
+//     }
+// }).then(data => {
+//     data.data.response.holidays.forEach(el => {
+//         console.log(el.name)
+//         console.log(el.date.iso)
+//     })
+// }).catch(err => {
+//     console.log(err)
+// })
