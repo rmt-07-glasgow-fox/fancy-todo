567fb00b86d85049ebf6a797c75346592b5f5084
agungmrizqon <73422796+agungmrizqon@users.noreply.github.com>
2021-01-09 17:35:16 +0700

    Revert "RM Agung M Rizqon S"

--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const router = require ("express").Router ()
-const UserController = require ("../controllers/userController")
-
-router.post ("/register", UserController.register)
-router.post ("/login", UserController.login)
-router.post ("/googleUser", UserController.loginGoogle)
-
-module.exports = router
