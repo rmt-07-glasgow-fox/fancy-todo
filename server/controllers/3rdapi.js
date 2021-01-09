7d4b2d28164291055906d1c28f2286b535157f8b
marcellasyw <marcella.septania0@gmail.com>
2021-01-09 16:03:26 +0700

    add 3rd api

--- server/controllers/3rdapi.js
+++ server/controllers/3rdapi.js
@@ -17,17 +17,20 @@ class apiController {
     }
 
     static async videos (req, res, next) {
-        let idArtist = Math.floor(Math.random() *145508)
-        let i 
-        if(idArtist <= 111233 && idArtist >= 145508) {
-            i = idArtist
-        } else {
-            idArtist += 100000
-            if(idArtist >= 145508){
-                i = 111352
-            }
-        }
-        console.log(i);
+        // let idArtist = Math.floor(Math.random() *145508)
+        let i = 111352
+        // if(idArtist <= 111233 && idArtist >= 145508) {
+        //     i = idArtist
+        //     console.log(i, 'i');
+        // } else {
+        //     idArtist += 100000
+        //     if(idArtist >= 145508){
+        //         i = 111352
+        //     } else {
+        //         i = idArtist
+        //     }
+        // }
+        // console.log(i);
         axios({
             method: 'GET',
             url: 'https://theaudiodb.com/api/v1/json/1/mvid.php',
@@ -36,7 +39,13 @@ class apiController {
             }
         })
         .then(data => {
-            res.status(200).json(data.data.mvids)
+            if (data.data.mvids.length >= 10) {
+                let index = Math.floor(Math.random() *data.data.mvids.length-10)
+                data.data.mvids = data.data.mvids.slice(index, index+10)
+                res.status(200).json(data.data.mvids)
+            } else {
+                res.status(200).json(data.data.mvids)
+            }
         })
         .catch(err => {
             next(err)
