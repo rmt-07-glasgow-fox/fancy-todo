b7c4bca302fde0cdf1ce3d504d5500c462da7cfd
Aldyco Turang <aldycoturang@gmail.com>
2021-01-09 15:15:46 +0700

    Login By Google

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,10 @@
-const { User } = require('../models')
+const { OAuth2Client } = require('google-auth-library')
 
+const { User } = require('../models')
 const { checkHash } = require ('../helpers/hashPassword')
 const { generateToken } = require('../helpers/jwt')
+
+const GOOGLE_CLIENT_ID =  process.env.GOOGLE_CLIENT_ID
 
 class Controller {
 
@@ -39,6 +42,53 @@ class Controller {
         } else {
           next({ name: 'Wrong Email' })
         }
+      })
+      .catch(err => {
+        next(err)
+      })
+  }
+
+  static loginByGoogle(req, res, next) {
+    let idToken = req.body.id_token
+    const client = new OAuth2Client(GOOGLE_CLIENT_ID)
+    let email
+
+    client.verifyIdToken({
+      idToken: id_token,
+      audience: GOOGLE_CLIENT_ID,
+    })
+      .then(ticket => {
+        let payload = ticket.getPayload();
+        email = payload.email
+
+        return User.findOne({
+          where: {
+            email
+          }
+        })
+      })
+      .then(data => {
+        if(!data) {
+          return User.create({
+            email,
+            password: "12345678"
+          })
+        } else {
+          return data
+        }
+      })
+      .then(data => {
+        let payload = {
+          id: data.id,
+          email: data.email
+        }
+        let access_token = generateToken(payload)
+
+        return res.status(200).json({
+          id: data.id,
+          email: data.email,
+          access_token
+        })
       })
       .catch(err => {
         next(err)
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user')
 
 router.post('/register', userController.registerUser)
 router.post('/login', userController.loginUser)
+router.post('/login/google', userController.loginByGoogle)
 
 
 module.exports = router
