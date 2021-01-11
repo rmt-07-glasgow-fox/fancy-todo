673b65edd5c8f6cb340fb9fd407e0e1fd92cd67e
Rivan-byte02 <rivanakbar02@gmail.com>
2021-01-12 04:57:52 +0700

    test

--- server/app.js
+++ server/app.js
@@ -9,13 +9,11 @@ const router = require('./routes')
 const errorHandlers = require('./middlewares/errorHandler')
 const cors = require('cors')
 
-app.use(cors());
-
 app.use(express.json());
 
-// app.set('view engine', 'ejs')
-
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors());
 
 app.use(router);
 
