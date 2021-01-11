c81b1f277ee8c4669749b76d61190a8aa156efb5
ObedAdhi <obedadhinatapraya@yahoo.com>
2021-01-11 23:09:07 +0700

    try deploy heroku

--- server/middlewares/middleware.js
+++ server/middlewares/middleware.js
@@ -12,40 +12,42 @@ const authentication = (req, res, next) => {
     let decoded = cekToken(req.headers.access_token)
     User.findOne({
       where: {
+        id: decoded.id,
         email: decoded.email
       }
     })
-    .then(data => {
-      if (!data) {
-        next({name: "notLoggedIn"})
-      } else {
-        req.user = data
-        next()
-      }
-    })
-    .catch(err => {
-      next(err)
-    })
-    
+      .then(data => {
+        console.log(decoded);
+        if (!data) {
+          next({ name: "notLoggedIn" })
+        } else {
+          req.user = data
+          next()
+        }
+      })
+      .catch(err => {
+        next(err)
+      })
+
   } catch (err) {
-    next({name: "notLoggedIn"})
+    next({ name: "notLoggedIn" })
   }
 }
 const authorization = (req, res, next) => {
   let id = req.params.id
   Todo.findByPk(id)
-  .then(data => {
-    if (!data) {
-      next({name: "resourceNotFound"})
-    } else if (data.userId == req.user.id) {
-      next()
-    } else {
-      next({name: "unauthorized"})
-    } 
-  })
-  .catch(err => {
-    next(err)
-  })
+    .then(data => {
+      if (!data) {
+        next({ name: "resourceNotFound" })
+      } else if (data.userId == req.user.id) {
+        next()
+      } else {
+        next({ name: "unauthorized" })
+      }
+    })
+    .catch(err => {
+      next(err)
+    })
 }
 
 
