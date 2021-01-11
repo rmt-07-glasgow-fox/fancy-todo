ce5f1e993e2df1caf965571183c6a247d4fd73d5
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-11 14:07:37 +0700

    [nanda] deploy heroku server

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV === "development") {
     require('dotenv').config()
 }
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const indexRouter = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
