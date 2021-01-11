f022e320b0eb00ac3c8b644cc9643be609a6b9f7
agungmrizqon <agungrizqon18@gmail.com>
2021-01-11 20:18:11 +0700

    tes fix

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require ("cors")
 const router = require("./routes")
 const { errorHandler } = require ("./middlewares/errorHandler")
 const app = express ()
-const port = 5000
+const port = process.env.PORT || 5000
 
 
 app.use (cors ())
