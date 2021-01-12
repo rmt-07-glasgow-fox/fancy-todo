82c293f96c6833a12d0ca6b7cca836c0b96e2651
Fauzi Ramadan <fauzidanrama@gmail.com>
2021-01-12 08:51:37 +0700

    final before deploy

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,10 @@ const authRouter = require('./auth.js')
 const { authenticate } = require('../middlewares/auth.js')
 const Controller = require('../controllers')
 
+router.get('/', Controller.getJoke)
 router.use(authRouter)
 
 router.use(authenticate)
 router.use('/todos', todosRouter)
-
-router.get('/', Controller.getJoke)
 
 module.exports = router
